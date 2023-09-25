acec282f9883c04b16e59ecd4da91a4f3df91968
Elonatech Nigeria limited <102363945+Elonatech@users.noreply.github.com>
2023-09-25 16:16:22 +0100

    update mongoose

--- config/db.js
+++ config/db.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const connectDb = async() => {
     // await mongoose.connect( "mongodb://localhost:27017/pfn_blog_data",
     // await mongoose.connect( "mongodb+srv://franklin:1234@cluster0.cckl6.mongodb.net/test",
+  mongoose.set('strictQuery', true);
    await mongoose.connect(process.env.MONGO_URI,
        {
          useNewUrlParser: true,
